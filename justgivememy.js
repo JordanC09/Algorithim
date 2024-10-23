30113c72cbd64b7f8e03697a7bcccd210edbc3ab
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2024-10-23 13:58:40 -0400

    be runnign

---
+++ justgivememy.js
@@ -0,0 +1,10 @@
+function stillcode(thosewhoknow) {
+  thosewhoknow.split("");
+  for (i = 0; i <= thosewhoknow.length(); i++) {
+    if (todaysplit[i] === "C" && yessplit[i] === "C") {
+      same += 1;
+    }
+  }
+}
+
+function justgivememydeci(mal) {}
--- occupyspaces.js
+++ occupyspaces.js
@@ -1,6 +1,22 @@
 function torchic(n, oggi, domani) {
-  for (i = 0; i <= n; i++) {}
+  let same = 0;
+  let todaysplit = oggi.split("");
+  let yessplit = domani.split("");
+  if (n < 0 || n % 1 != 0) {
+    return;
+  } else {
+    for (i = 0; i <= n; i++) {
+      if (todaysplit[i] === "C" && yessplit[i] === "C") {
+        same += 1;
+      }
+    }
+    return same;
+  }
 }
+let oggi = "C.CC..";
+let domani = ".CC.C";
+let n = 5;
+console.log(torchic(n, oggi, domani));
 
 //n = cars in lot
 // oggi = string that represents either C or .
