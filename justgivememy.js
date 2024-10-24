8373d8a51b45a3934af1c379769cfb8fab958a24
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2024-10-24 13:59:10 -0400

    no

--- justgivememy.js
+++ justgivememy.js
@@ -1,10 +1,26 @@
 function stillcode(thosewhoknow) {
   thosewhoknow.split("");
-  for (i = 0; i <= thosewhoknow.length(); i++) {
-    if (todaysplit[i] === "C" && yessplit[i] === "C") {
-      same += 1;
+  let eyad = thosewhoknow.length;
+
+  let iamsteve = 0;
+  let fein = 1;
+  for (i = 0; i < eyad; i++) {
+    if (thosewhoknow[eyad - i] === "1") {
+      fein = fein + Math.pow(2, iamsteve);
+    }
+    iamsteve += 1;
+  }
+  return fein;
+}
+console.log(stillcode("11111111"));
+
+function justgivememybi(nary) {
+  let bi = "";
+  for (i = 8; i > -1; i --){
+    if ((nary % (Math.pow(2,i))) = 0 ){
+      bi += "1";
     }
   }
+  return bi;
 }
-
-function justgivememydeci(mal) {}
+console.log(justgivememybi(11));
